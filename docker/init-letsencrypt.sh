9f29e668326799903e1ed73d60b35f8821862989
gimmyhat <gimmyhat@gmail.com>
2025-03-20 19:32:14 +0800

    fix: обновлена конфигурация SSL для nginx и certbot

--- docker/init-letsencrypt.sh
+++ docker/init-letsencrypt.sh
@@ -3,39 +3,41 @@
 # Домен для сертификата
 domains=(krovlya38.pro www.krovlya38.pro)
 rsa_key_size=4096
-data_path="./docker/certbot"
+data_path="/etc/letsencrypt"
 email="admin@krovlya38.pro" # Замените на свой email
 
-# Создаем директории для certbot
-mkdir -p $data_path/conf/live/$domains
-mkdir -p $data_path/www
+# Создаем директорию для certbot и настраиваем временные файлы
+mkdir -p "$data_path/live/krovlya38.pro"
+mkdir -p /var/www/certbot
 
-# Остановим и удалим старые контейнеры
-docker compose -f docker/docker-compose.yml down
-
-# Удалим старые сертификаты
-rm -rf $data_path/conf/live/$domains
-
-# Создадим временный самоподписанный сертификат для запуска Nginx
-mkdir -p "$data_path/conf/live/$domains"
+# Создаем временный самоподписанный сертификат
 openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1 \
-  -keyout "$data_path/conf/live/$domains/privkey.pem" \
-  -out "$data_path/conf/live/$domains/fullchain.pem" \
-  -subj "/CN=$domains"
+  -keyout "$data_path/live/krovlya38.pro/privkey.pem" \
+  -out "$data_path/live/krovlya38.pro/fullchain.pem" \
+  -subj "/CN=${domains[0]}"
 
-# Создадим опции SSL для Nginx
-curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
-curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+# Создаем файлы конфигурации SSL для Nginx
+echo "ssl_protocols TLSv1.2 TLSv1.3;" > "$data_path/options-ssl-nginx.conf"
+echo "ssl_prefer_server_ciphers off;" >> "$data_path/options-ssl-nginx.conf"
+echo "ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;" >> "$data_path/options-ssl-nginx.conf"
+echo "ssl_session_timeout 1d;" >> "$data_path/options-ssl-nginx.conf"
+echo "ssl_session_cache shared:SSL:10m;" >> "$data_path/options-ssl-nginx.conf"
+echo "ssl_session_tickets off;" >> "$data_path/options-ssl-nginx.conf"
 
-# Запустим контейнеры
-docker compose -f docker/docker-compose.yml up -d
+# Создаем DH параметры
+openssl dhparam -out "$data_path/ssl-dhparams.pem" 2048
 
-# Дождемся запуска Nginx
-echo "Ожидаем запуска Nginx..."
+echo "Созданы временные SSL сертификаты и конфигурация"
+
+# Перезапускаем контейнеры
+docker compose down
+docker compose up -d nginx
+
+echo "Nginx запущен. Подождите немного для инициализации."
 sleep 5
 
-# Запросим сертификат Let's Encrypt
-docker compose -f docker/docker-compose.yml run --rm certbot certonly --webroot \
+# Запускаем certbot для получения реальных сертификатов
+docker compose run --rm certbot certonly --webroot \
   --webroot-path=/var/www/certbot \
   --email $email \
   --agree-tos \
@@ -43,7 +45,7 @@ docker compose -f docker/docker-compose.yml run --rm certbot certonly --webroot 
   --force-renewal \
   -d ${domains[0]} -d ${domains[1]}
 
-# Перезапустим Nginx
-docker compose -f docker/docker-compose.yml exec nginx nginx -s reload
+echo "Пытаемся перезапустить Nginx для применения сертификатов"
+docker compose exec nginx nginx -s reload
 
-echo "Поздравляем! HTTPS настроен для $domains" 
+echo "Процесс получения SSL-сертификатов завершен. Проверьте статус сертификатов." 
